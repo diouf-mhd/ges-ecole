3799fecb171e4f5defab358e7e4f3cd7b12b2e6b
moussa diouf <dioufmoussa2.0@outlook.com>
2026-09-19 10:51:53 +0000

    first commit

---
+++ src/main/java/com/example/ges_ecole/GesEcoleApplication.java
@@ -0,0 +1,13 @@
+package com.example.ges_ecole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesEcoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesEcoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ges_ecole/GesEcoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ges_ecole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesEcoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
